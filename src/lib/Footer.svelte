eaacdf3e07520114406f8670bf11ee01537ff1f4
Hunter McQuaid <89223779+amgosek@users.noreply.github.com>
2024-04-15 21:54:21 -0400

    added content to footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,4 +1,19 @@
+<script>
+    import WordMark from "./WordMark.svelte";
+    import {menuItems} from '$lib/menuItems';
+</script>
+
 <footer class="bg-gray-700 text-white p-4 text-center">
-    <p>© 2024 My Website. All rights reserved.</p>
-  </footer>
+    <div class="flex justify-between items-center w-full">
+        <a href="/">
+            <WordMark />
+        </a>
+        <p>© 2024 My Website. All rights reserved.</p>
+        <ul class="flex space-x-4">
+            {#each menuItems as { text, href }}
+              <li><a href={href} class="hover:text-gray-300">{text}</a></li>
+            {/each}
+        </ul>
+    </div>
+</footer>
   
