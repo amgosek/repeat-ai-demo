e356ad1f8d730333fbd303b5c9960378a1155193
Hunter McQuaid <89223779+amgosek@users.noreply.github.com>
2024-04-19 05:37:56 -0400

    Added showcase

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
+	import Showcase from "$lib/Showcase/Showcase.svelte";
 import Bento from "../lib/Bento/Bento.svelte";
 import Hero from "../lib/Hero/Hero.svelte";
 </script>
 
 <Hero />
 <Bento />
+<Showcase />
