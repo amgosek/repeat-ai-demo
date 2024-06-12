87f179d91b1aa9348d059b40acc63fd11c2f0e27
Hunter McQuaid <89223779+amgosek@users.noreply.github.com>
2024-06-11 20:13:51 -0400

    added new section to homepage and updated depends

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Showcase from "$lib/Showcase/Showcase.svelte";
-import Bento from "../lib/Bento/Bento.svelte";
-import Hero from "../lib/Hero/Hero.svelte";
+	import Bento from "../lib/Bento/Bento.svelte";
+	import Hero from "../lib/Hero/Hero.svelte";
 </script>
 
 <Hero />
